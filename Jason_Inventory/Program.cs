84f5672261187709d518e943584214af4e6e313d
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-09 20:28:19 +0530

    [Abhi]Added:Jason inventory management

---
+++ Jason_Inventory/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Jason_Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath= "C:\\Users\\Administrator\\Desktop\\Day11-12_Jason\\Jason_Inventory\\InventoryData.json";
+
+            fetchforjasonRiceData fetchforjasonRiceData = new fetchforjasonRiceData();
+            Rice data = fetchforjasonRiceData.Read(filepath);
+
+            //Console.WriteLine(data.typeofRice.Name);
+            //Console.WriteLine(data.typeofRice.price);
+            //Console.WriteLine(data.typeofRice.weight);
+            Console.WriteLine("data for Rice");
+
+            for (int i = 0; i < data.typeofRice.Count; i++)
+            {
+                Console.WriteLine(data.typeofRice[i].Name);
+                Console.WriteLine(data.typeofRice[i].price);
+                Console.WriteLine(data.typeofRice[i].weight);
+                Console.WriteLine("-----------------");
+            }
+            Console.WriteLine("Data for Pulses");
+
+            for (int i = 0; i < data.typeofPulse.Count; i++)
+            {
+                Console.WriteLine(data.typeofPulse[i].Name);
+                Console.WriteLine(data.typeofPulse[i].price);
+                Console.WriteLine(data.typeofPulse[i].weight);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("Data for Wheat");
+
+            for (int i = 0; i < data.typeofWheat.Count; i++)
+            {
+                Console.WriteLine(data.typeofWheat[i].Name);
+                Console.WriteLine(data.typeofWheat[i].price);
+                Console.WriteLine(data.typeofWheat[i].weight);
+                Console.WriteLine("-----------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
---
+++ Jason_Inventory/Rice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jason_Inventory
+{
+    internal class Rice
+    {
+        public List<TypeofRice> typeofRice;
+        public List<TypeofPulse> typeofPulse;
+        public List<TypeofWheat> typeofWheat;
+    }
+    public class TypeofRice
+    {
+        //Properties of Rice
+        public string Name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+    public class TypeofPulse
+    {
+        //Properties of pulse
+        public string Name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+    public class TypeofWheat
+    {
+        //Properties of Rice
+        public string Name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+}
+
